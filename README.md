ab38f0e50f50eaf7db17055c55fe2deac50b42ed
sv2421 <sv2421@columbia.edu>
2017-01-24 16:04:43 -0500

    finished writing first draft of likelihood

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ Currently seeing if we're going to use Astrochem or its python wrapper.
 Of course we would like to use the python wrapper, but our first bench-
 marking results suggest that the wrapper isn't giving the right results.
 
+UPDATE: 1/24/17, the wrapper is working, we just didn't know the times
+were in terms of seconds on the solver.
+
 Our goal is to use astrochem output and observed abundances in TMC-1 to
 infer input parameters like temperature, density, CRIR, Av, and age.
--- astrochem_wrapper.py
+++ astrochem_wrapper.py
@@ -1,12 +1,48 @@
 from astrochem.wrapper import *
 import numpy as np
-import matplotlib.pyplot as plt
 
-p = phys()
-p.chi = 1
-p.cosmic = 1.3e-17
+def loglike(T, nH, time):
+    """
+    Given a temperature, density, and time, returns the log likelihood
+    of those inputs giving observed abundances for a preset list of
+    astrochemical species. TODO: figure out errors so we don't
+    assume homoscedasticity.
 
-initial_abundances = {
+    Args:
+        T (float): the temperature of the dark cloud
+        nH (float): the number density of hydrogen nuclei in the cloud
+        time (float): the age of the cloud
+    """
+
+    specs = ['HCN', 'HCO(+)', 'C2H3N', 'CO']
+    obser = np.log10(np.array([2e-8, 8e-9, (10**12.61)/1e22, 8e-5]))
+    #error = np.log10(np.array([2e-8, 8e-9, (10**12.61)/1e22, 8e-5]))
+    calcu = np.log10(np.array(get_abundances(T, nH, time, specs)))
+    resid = obser - calcu
+    #chisq = (resid/error)**2
+    chisq = resid**2
+    return -0.5*sum(chisq)
+
+def get_abundances(T, nH, time, specs):
+    """
+    Given a set of input parameters and species, this function
+    calls the astrochem model and returns abundances for those
+    species.
+
+    Args:
+        T (float): the temperature of the dark cloud
+        nH (float): the number density of hydrogen nuclei in the cloud
+        time (float): the age of the cloud
+        specs (list of str): a list of species to calculate abundances for
+    """
+
+    #setting physical conditions in the cell
+    p = phys()
+    p.chi = 1
+    p.cosmic = 1.3e-17
+
+    #setting initial abundances
+    initial_abundances = {
 "H2"      : 5.00e-1,
 "He"      : 1.40e-1,
 "N"       : 2.14e-5,
@@ -22,27 +58,27 @@ initial_abundances = {
 "F"       : 2.00e-8,
 "e(-)"    : 7.303500e-05}
 
-density = 1e4
-av = 20
-tgas = 10
-tdust = 10
-c = cell(av, density, tgas, tdust)
-verbose = 0
-abs_err = 1e-20
-rel_err = 1e-6
-s = solver(c, "osu2009.chm", p, abs_err, rel_err, initial_abundances, density, verbose)
-times = np.logspace(2, 8, 128)
-abundances = []
-for time in times:
+    #setting more physical conditions in the cell
+    density = nH
+    av = 20
+    tgas = T
+    tdust = T
+    c = cell(av, density, tgas, tdust)
+
+    #setting up the solver
+    verbose = 0
+    abs_err = 1e-20
+    rel_err = 1e-6
+    s = solver(c, "osu2009.chm", p, abs_err, rel_err, initial_abundances, density, verbose)
+
+    #solving for the abundances
+    abundances = []
     try:
-        abundances.append(s.solve(time * 3600 * 24 * 365)['CO'])
+        abun = s.solve(time * 3600 * 24 * 365)
+        for spec in specs:
+            abundances.append(abun[spec])
     except ArithmeticError as e:
         raise "Something went wrong: %s" % e
+    return abundances
 
-plt.plot(times, abundances)
-plt.title('Wrapper-Generated Figure')
-plt.ylabel('Abundance')
-plt.xlabel('Time')
-plt.xscale('log')
-plt.yscale('log')
-plt.savefig('wrapper_figure.png')
+print loglike(10, 1e4, 1e5)
